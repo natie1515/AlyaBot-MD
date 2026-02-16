516254a7b90083a65d64b5bbd21d352cb62fc585
Sarah <frejulian57@gmail.com>
2026-02-16 00:09:24 -0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -23,7 +23,7 @@ global.mods = [
 ]
 
 global.msgglobal = '✿⸝꙳.˖ Ocurrió un problema, contacte al creador'
-global.dev = `ʙᴜɪʟᴛ ᴀɴᴅ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ɪ'ᴍ ᴅɪᴇɢᴏ ッ`
+global.dev = `ʙᴜɪʟᴛ ᴀɴᴅ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ɪ'ᴍ ⁿᵏGwee⚝  ッ`
 
 global.mess = {
   socket: '(∩´͈ ᴖ `͈∩ ྀི) Este comando solo puede ser ejecutado por un Socket.',
