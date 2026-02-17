c5c4dac1d9ccd811b51082d1a57cc581895be9ff
Sarah <frejulian57@gmail.com>
2026-02-17 17:09:00 -0400

    Update botcount.js

--- commands/owner/botcount.js
+++ commands/owner/botcount.js
@@ -9,23 +9,30 @@ export default {
       if (!code) return m.reply("Link inválido");
 
       const group = await client.groupGetInviteInfo(code);
-      const bots = [client, ...(global.conns || [])];
 
+      const bots = [client, ...(global.conns || [])];
       let total = 0;
 
       for (let conn of bots) {
         try {
-          await conn.groupMetadata(group.id);
-          total++;
+          const metadata = await conn.groupMetadata(group.id);
+          const botID =
+            conn.user.id.split(":")[0] + "@s.whatsapp.net";
+
+          const esta = metadata.participants.some(
+            p => p.id === botID
+          );
+
+          if (esta) total++;
         } catch {}
       }
 
-      if (total === 0)
+      if (!total)
         return m.reply("⚠️ No hay bots en ese grupo");
 
-      m.reply(`🤖 Bots dentro del grupo: ${total}`);
+      m.reply(`🤖 Bots en el grupo: ${total}`);
     } catch (e) {
-      m.reply("⚠️ No pude verificar el grupo");
+      m.reply("No pude obtener info del grupo");
     }
   }
 };
