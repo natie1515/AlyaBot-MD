ffb11e1bf63cfc606b6134746c3524864ad387b5
Sarah <frejulian57@gmail.com>
2026-02-17 17:31:05 -0400

    Update salirgrupo.js

--- commands/owner/salirgrupo.js
+++ commands/owner/salirgrupo.js
@@ -12,23 +12,32 @@ export default {
       if (!code) return m.reply("Link inválido");
 
       const group = await client.groupGetInviteInfo(code);
+      const bots = global.conns || [];
 
       m.reply("Sacando SubBots del grupo...");
 
-      // SOLO subbots
-      const bots = global.conns || [];
       let salieron = 0;
 
       for (let conn of bots) {
         try {
-          await conn.groupLeave(group.id);
-          salieron++;
-          await new Promise(r => setTimeout(r, 800));
+          const meta = await conn.groupMetadata(group.id);
+          const botID =
+            conn.user.id.split(":")[0] + "@s.whatsapp.net";
+
+          const esta = meta.participants.some(
+            p => p.id === botID
+          );
+
+          if (esta) {
+            await conn.groupLeave(group.id);
+            salieron++;
+            await new Promise(r => setTimeout(r, 800));
+          }
         } catch {}
       }
 
       if (!salieron)
-        return m.reply("⚠️ No había subbots en el grupo");
+        return m.reply("⚠️ No hay SubBots en el grupo");
 
       m.reply(`✅ ${salieron} SubBots salieron del grupo`);
     } catch (e) {
