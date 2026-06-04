b331951330199491a4f268b9c37f35a9f1c4787b
ɪ'ᴍ ᴅɪᴇɢᴏ ᜊ•ᴗ•ᜊ <sarmientodieg@gmail.com>
2026-06-04 10:34:11 -0500

    Actualizar chatgpt.js

--- cmds/ai/chatgpt.js
+++ cmds/ai/chatgpt.js
@@ -1,4 +1,4 @@
-import {getUser, updateUser, getChat, updateChat, getChatUser, updateChatUser, getSettings, updateSettings, getStickersPack, updateStickersPack, deletedb, setCreate} from "#database"
+import { getSettings, deletedb, setCreate } from "#database"
 import fetch from 'node-fetch';
 
 export default {
@@ -29,27 +29,7 @@ export default {
 
       const response = `${json.result}`.trim()
 
-      const userAskedCode = /(codigo|code|programa|script|actualiza|edita)/i.test(text)
-
-      const looksLikeCode = /function|class|const|let|var|=>|\{|\}|console\.log/i.test(response)
-
-      if (userAskedCode || looksLikeCode) {
-        let language = 'javascript'
-        if (/typescript/i.test(text) || /typescript/i.test(response)) language = 'typescript'
-        else if (/python/i.test(text) || /def |import |print\(/i.test(response)) language = 'python'
-        else if (/html/i.test(text) || /<html>|<div>|<span>/i.test(response)) language = 'html'
-        else if (/css/i.test(text) || /\{.*\}/i.test(response) && /color|margin|padding|font/i.test(response)) language = 'css'
-
-        await sock.sendCodeMessage(
-          msg.chat,
-          dev,
-          language,
-          response,
-          msg
-        )
-      } else {
         await sock.sendMessage(msg.chat, { text: response, edit: key })
-      }
     } catch (error) {
       console.error(error)
       await msg.reply(msgglobal)
